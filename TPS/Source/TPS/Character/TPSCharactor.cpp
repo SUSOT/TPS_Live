1d831059e67bfd214969ede5e6c986c2fccef495
SUSOT <a01076754042@gmail.com>
2025-05-20 20:00:13 +0900

    Feat : Complete Input System

--- TPS/Source/TPS/Character/TPSCharactor.cpp
+++ TPS/Source/TPS/Character/TPSCharactor.cpp
@@ -4,12 +4,17 @@
 #include "Character/TPSCharactor.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
+#include "InputMappingContext.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
 // Sets default values
 ATPSCharactor::ATPSCharactor()
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	bUseControllerRotationYaw = true;
 
 	// Mesh
 	GetMesh()->SetRelativeLocation(FVector(0.0f, 0.0f, -88.0f));
@@ -27,17 +32,56 @@ ATPSCharactor::ATPSCharactor()
 	SpringArm->SetupAttachment(GetRootComponent());
 	SpringArm->TargetArmLength = 500.0f;
 	SpringArm->SetRelativeRotation(FRotator(-30.0f, 0.0f, 0.0f));
+	SpringArm->bUsePawnControlRotation = true;
+	SpringArm->SocketOffset = FVector(0.0f, 50.0f, 50.0f);
 
 	// Camera
 	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
 	Camera->SetupAttachment(SpringArm);
+
+#pragma region InputSystem
+	static ConstructorHelpers::FObjectFinder<UInputMappingContext> IMCDefaultRef(TEXT("/Script/EnhancedInput.InputMappingContext'/Game/Input/IMC_Default.IMC_Default'"));
+
+	if (IMCDefaultRef.Succeeded())
+	{
+		IMCDefault = IMCDefaultRef.Object;
+	}
+
+	static ConstructorHelpers::FObjectFinder<UInputAction> JumpActionRef(TEXT("/Script/EnhancedInput.InputAction'/Game/Input/Actions/IA_Jump.IA_Jump'"));
+	if (JumpActionRef.Succeeded())
+	{
+		JumpAction = JumpActionRef.Object;
+	}
+	static ConstructorHelpers::FObjectFinder<UInputAction> MoveActionRef(TEXT("/Script/EnhancedInput.InputAction'/Game/Input/Actions/IA_Move.IA_Move'"));
+	if (MoveActionRef.Succeeded())
+	{
+		MoveAction = MoveActionRef.Object;
+	}
+	static ConstructorHelpers::FObjectFinder<UInputAction> TurnActionRef(TEXT("/Script/EnhancedInput.InputAction'/Game/Input/Actions/IA_Turn.IA_Turn'"));
+	if (TurnActionRef.Succeeded())
+	{
+		TurnAction = TurnActionRef.Object;
+	}
+#pragma endregion
+
 }
+
 
 // Called when the game starts or when spawned
 void ATPSCharactor::BeginPlay()
 {
 	Super::BeginPlay();
 
+	APlayerController* PlayerController = Cast<APlayerController>(GetController());
+	if (PlayerController)
+	{
+		UEnhancedInputLocalPlayerSubsystem* SubSystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
+
+		if (SubSystem)
+		{
+			SubSystem->AddMappingContext(IMCDefault, 0);
+		}
+	}
 }
 
 // Called every frame
@@ -52,5 +96,30 @@ void ATPSCharactor::SetupPlayerInputComponent(UInputComponent* PlayerInputCompon
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent);
+	if (EnhancedInputComponent)
+	{
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ACharacter::Jump);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ATPSCharactor::Input_Move);
+		EnhancedInputComponent->BindAction(TurnAction, ETriggerEvent::Triggered, this, &ATPSCharactor::Input_Turn);
+	}
+
+}
+
+void ATPSCharactor::Input_Move(const FInputActionValue& InputValue)
+{
+	const FVector2D MovementVector = InputValue.Get<FVector2D>();
+
+	AddMovementInput(GetActorForwardVector(), MovementVector.X);
+	AddMovementInput(GetActorRightVector(), MovementVector.Y);
+}
+
+void ATPSCharactor::Input_Turn(const FInputActionValue& InputValue)
+{
+	const FVector2D LookAxisVector = InputValue.Get<FVector2D>();
+
+	AddControllerYawInput(LookAxisVector.X);
+	AddControllerPitchInput(LookAxisVector.Y);
 }
 
